34e169f05f2cbe0877a5c5472fd18709fe4cae75
ArthurSantos54 <Arthursantos4367@gmail.com>
2022-01-19 18:49:06 -0300

    Aula de hoje

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-window.alert("Olá mundo");
+alert(screen.width)
+
+document.getElementById("largura").innerHTML = screen.width
+
+function voltar() {
+    history.back()
+}
+
+function adiantar() {
+    history.forward()
+}
+
+console.log(
+    screen.width
+)
